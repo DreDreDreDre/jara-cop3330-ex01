0e61e92909bb29a4bd88024877858af8175a6dab
DreDreDreDre <andrejar34@gmail.com>
2021-09-09 21:33:09 -0400

    Exercise 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,12 @@
 package org.example;
 import java.sql.SQLOutput;
 import java.util.Scanner;
-/**
- * Hello world!
- *
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Andre Jara
  */
+
 public class App 
 {
     public static void main( String[] args )
